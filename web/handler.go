c0decf77dac56e19485af34bbfacfa1b7bc078b8
enjaku4 <enjaku4@gmail.com>
2021-11-19 16:51:23 +0200

    html playground

--- web/handler.go
+++ web/handler.go
@@ -25,6 +25,26 @@ func NewHandler(store goreddit.Store) *Handler {
 		r.Post("/delete/{id}", h.ThreadsDelete())
 	})
 
+	h.Get("/html", func(w http.ResponseWriter, r *http.Request) {
+		t := template.Must(template.ParseFiles("templates/layout.html", "templates/child_template.html", "templates/includes/paragraph.html"))
+
+		type params struct {
+			Title   string
+			Text    string
+			Lines   []string
+			Number1 int
+			Number2 int
+		}
+
+		t.Execute(w, params{
+			Title:   "Reddit clone",
+			Text:    "Welcome to our Reddit clone",
+			Lines:   []string{"Line1", "Line2", "Line3"},
+			Number1: 42,
+			Number2: 23534,
+		})
+	})
+
 	return h
 }
 
